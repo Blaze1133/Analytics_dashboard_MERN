79b334e0ef68ea6a9d98c56fb98fc8f63c12d707
Blaze <manikanta3.mk1@gmail.com>
2025-08-21 20:38:24 +0530

    env modified

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: "adminApi", // Fixed typo
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
   tagTypes: [
     "User",
     "Products",
